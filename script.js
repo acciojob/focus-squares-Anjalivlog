659dd03df3b389ece0ae683434c6f8ce72f6f1d7
Anjalivlog <139326744+Anjalivlog@users.noreply.github.com>
2024-04-03 21:32:36 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
 //your JS code here. If required.
+const squares = document.querySelectorAll('.square');
+squares.forEach(function(square) {
+  square.addEventListener('mouseover', function(event) {
+    const currentSquare = event.target;
+    
+    squares.forEach(function(sq) {
+      sq.style.backgroundColor = '#6F4E37';
+    });
+	  
+    currentSquare.style.backgroundColor = '#E6E6FA';
+  });
+
+  
+  square.addEventListener('mouseout', function() {
+    squares.forEach(function(sq) {
+      sq.style.backgroundColor = '#E6E6FA';
+    });
+  });
+});
